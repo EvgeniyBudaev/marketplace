5febcded3516d6142a6edb0eb0defa6bf5fc2ebe
Evgeniy Budaev <budaev.e@gmail.com>
2022-11-20 08:50:15 +0300

    feat: frontend. tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+function withOpacityValue(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `rgb(var(${variable}))`;
+    }
+    return `rgb(var(${variable}), ${opacityValue})`;
+  };
+}
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: withOpacityValue('--color-primary'),
+      }
+    },
+  },
+  plugins: [],
+}
