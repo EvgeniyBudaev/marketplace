5141698328dc7983537ecd73842ec98cfb743d3a
Evgeniy Budaev <budaev.e@gmail.com>
2022-11-20 10:34:43 +0300

    feat: frontend. delete tailwind. add button, styles

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,5 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world</h1>
-        <div className="text-primary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, consequatur!</div>
-    </div>
-  )
+import {Home} from "../components";
+
+export default function HomePage() {
+  return <Home />;
 }
