927ba5e7a6e5992132d1b25751a1b2ddea43c8c0
Evgeniy Budaev <budaev.e@gmail.com>
2022-12-01 08:41:32 +0300

    feat: frontend. NavLink

---
+++ frontend/src/components/NavLink/NavLink.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {cloneElement} from "react"
+import type {FC} from "react";
+import { ReactElementLike } from "prop-types";
+
+type TProps = {
+    activeClassName?: string;
+    href: string;
+    children: ReactElementLike;
+};
+
+export const NavLink: FC<TProps> = ({
+                                                     activeClassName = "isActive",
+                                                     href,
+                                                     children,
+                                                 }) => {
+    const pathname = usePathname();
+
+    let className = children.props.className || "";
+
+    if (pathname === href) {
+        className = `${className} ${activeClassName}`;
+    }
+
+    return <Link href={href}>{cloneElement(children, { className })}</Link>;
+};
