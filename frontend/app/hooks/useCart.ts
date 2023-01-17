a8c21764e1750b3a45d1dbac1ee256ac54063cd3
Evgeniy Budaev <budaev.e@gmail.com>
2023-01-17 17:07:18 +0300

    feat: frontend. fixed cart

--- frontend/app/hooks/useCart.ts
+++ frontend/app/hooks/useCart.ts
@@ -1,10 +1,8 @@
 import { useCallback } from "react";
 import { useFetcher } from "@remix-run/react";
-import { useStoreContext } from "~/shared/store";
 
 export const useCart = () => {
   const fetcher = useFetcher();
-  const { cart } = useStoreContext();
 
-  return { cart };
+  return null;
 };
