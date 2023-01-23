44d9115204fdcacddb21f535f039f7fb5e342aca
winterom <winterom@gmail.com>
2023-01-23 16:45:21 +0300

    Промежуток

--- backend/src/main/java/com/marketplace/backend/mappers/SelectableValueMapper.java
+++ backend/src/main/java/com/marketplace/backend/mappers/SelectableValueMapper.java
@@ -5,6 +5,7 @@ import com.marketplace.backend.model.values.SelectableValue;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+
 import java.util.List;
 
 @Mapper
@@ -12,6 +13,7 @@ public interface SelectableValueMapper {
 
     List<SelectableValue> dtoListToEntityList(List<SelectableValueDto> dtoList);
     SelectableValue dtoToEntity(SelectableValueDto dto);
+    @Mapping(target = "attributeId",source = "attribute.id")
     SelectableValueDto entityToDto(SelectableValue selectableValue);
     List<SelectableValueDto> entityListToDtoList(List<SelectableValue> values);
 }
