59024003fc96b0f0b4653a037fcd02e08ff11537
Evgeniy Budaev <budaev.e@gmail.com>
2023-02-01 19:09:56 +0300

    feat: frontend. theme select, switcher

--- frontend/app/shared/store/hooks/useStore.ts
+++ frontend/app/shared/store/hooks/useStore.ts
@@ -1,7 +1,13 @@
 import { useState } from "react";
+import { TSettings } from "~/shared/api/settings";
 
 export const useStore = () => {
-  const [settings, setSettings] = useState<any>();
+  const [settings, setSettings] = useState<any>({
+    currency: "",
+    language: "",
+    theme: "",
+    uuid: "",
+  });
   const [user, setUser] = useState<any>();
 
   return { settings, setSettings, user, setUser };
--- frontend/app/shared/store/types.ts
+++ frontend/app/shared/store/types.ts
@@ -1,8 +1,9 @@
 import type { Context, Dispatch, SetStateAction } from "react";
+import { TSettings } from "~/shared/api/settings";
 
 export type TStore = {
-  settings: any;
-  setSettings: Dispatch<SetStateAction<Context<any>>>;
+  settings: TSettings;
+  setSettings: Dispatch<SetStateAction<Context<TSettings>>>;
   user: any;
   setUser: Dispatch<SetStateAction<Context<any>>>;
 };
