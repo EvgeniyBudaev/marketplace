699bdd91d5972df85674b990bc641c947ff26b68
Evgeniy Budaev <budaev.e@gmail.com>
2023-02-24 14:27:03 +0300

    fix: frontend. fixed attributes crud

--- frontend/app/routes/admin/products/add/index.tsx
+++ frontend/app/routes/admin/products/add/index.tsx
@@ -1,9 +1,12 @@
-import { inputFromForm } from "remix-domains";
+import { inputFromForm, inputFromSearch } from "remix-domains";
 import { json } from "@remix-run/node";
-import type { ActionArgs } from "@remix-run/node";
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { ProductAdd, productAddLinks } from "~/pages/Admin/Products/ProductAdd";
+import { getCatalogs } from "~/shared/api/catalogs";
+import { mapParamsToDto } from "~/shared/api/catalogs/utils";
 import { addProduct } from "~/shared/api/products";
 import { internalError } from "~/utils";
+import { useLoaderData } from "@remix-run/react";
 
 export const action = async (args: ActionArgs) => {
   const { request } = args;
@@ -21,8 +24,29 @@ export const action = async (args: ActionArgs) => {
   });
 };
 
+export const loader = async (args: LoaderArgs) => {
+  const { request } = args;
+  const url = new URL(request.url);
+  const formValues = inputFromSearch(url.searchParams);
+
+  const formattedParams = mapParamsToDto({
+    ...formValues,
+  });
+  console.log("[catalogs formattedParams: ] ", formattedParams);
+  const response = await getCatalogs(request, { params: formattedParams });
+
+  if (!response.success) {
+    throw internalError();
+  }
+  console.log("response.data: ", response.data);
+  return json({
+    catalogs: response.data,
+  });
+};
+
 export default function ProductAddRoute() {
-  return <ProductAdd />;
+  const data = useLoaderData<typeof loader>();
+  return <ProductAdd catalogs={data.catalogs} />;
 }
 
 export function links() {
