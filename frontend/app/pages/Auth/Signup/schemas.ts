509e1d438a55b715e0ad9db9eb784c1292ec7b71
Evgeniy Budaev <budaev.e@gmail.com>
2023-04-09 18:26:43 +0300

    feat: frontend. signup phone

--- frontend/app/pages/Auth/Signup/schemas.ts
+++ frontend/app/pages/Auth/Signup/schemas.ts
@@ -6,13 +6,26 @@ import {
   EMAIL_NOT_CYRILLIC_REGEXP,
   EMAIL_REGEXP,
   EMPTY_FIELD_ERROR_MESSAGE,
+  NAME_ERROR_MESSAGE,
+  NAME_REGEXP,
+  PASSWORD_ERROR_MESSAGE,
+  PHONE_ERROR_MESSAGE,
+  PHONE_REGEXP,
 } from "~/shared/validation";
 
 export const formSchema = z
   .object({
-    [EFormFields.FirstName]: z.string().trim().min(1, EMPTY_FIELD_ERROR_MESSAGE),
-    [EFormFields.LastName]: z.string().trim().min(1, EMPTY_FIELD_ERROR_MESSAGE),
-    [EFormFields.MiddleName]: z.string().trim().optional(),
+    [EFormFields.FirstName]: z
+      .string()
+      .trim()
+      .min(1, EMPTY_FIELD_ERROR_MESSAGE)
+      .regex(NAME_REGEXP, NAME_ERROR_MESSAGE),
+    [EFormFields.LastName]: z
+      .string()
+      .trim()
+      .min(1, EMPTY_FIELD_ERROR_MESSAGE)
+      .regex(NAME_REGEXP, NAME_ERROR_MESSAGE),
+    [EFormFields.MiddleName]: z.string().trim().regex(NAME_REGEXP, NAME_ERROR_MESSAGE).optional(),
     [EFormFields.Phone]: z.string().trim().min(11, EMPTY_FIELD_ERROR_MESSAGE),
     [EFormFields.Email]: z
       .string()
@@ -24,17 +37,26 @@ export const formSchema = z
     [EFormFields.Password]: z.string().trim().min(1, EMPTY_FIELD_ERROR_MESSAGE),
     [EFormFields.RePassword]: z.string().trim().min(1, EMPTY_FIELD_ERROR_MESSAGE),
   })
-  .superRefine(({ password, rePassword }, ctx) => {
+  .superRefine(({ phone, password, rePassword }, ctx) => {
     if (password !== rePassword) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
         path: [EFormFields.Password],
-        message: "The password did not match",
+        message: PASSWORD_ERROR_MESSAGE,
       });
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
         path: [EFormFields.RePassword],
-        message: "The confirm password did not match",
+        message: PASSWORD_ERROR_MESSAGE,
+      });
+    }
+
+    if (phone && !PHONE_REGEXP.test(phone)) {
+      console.log("PHONE_REGEXP", !PHONE_REGEXP.test(phone));
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: [EFormFields.Phone],
+        message: PHONE_ERROR_MESSAGE,
       });
     }
   });
--- frontend/app/utils/i18next/stringifyTranslationParams.ts
+++ frontend/app/utils/i18next/stringifyTranslationParams.ts
@@ -1,8 +1,1 @@
-import type { TFunction } from "i18next";
-
-function stringifyTranslationParams(...params: Parameters<TFunction>): string {
-  return JSON.stringify(params);
-}
-
-// export const t: TFunction = stringifyTranslationParams;
-export const t: (...params: Parameters<TFunction>) => string = stringifyTranslationParams;
+export const t = (path: string, options?: any) => path;
