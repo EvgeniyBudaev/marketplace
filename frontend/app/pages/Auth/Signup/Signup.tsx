eb72f7bfd44cbac4e29ce1c3080fe0f3f4baf6b2
Evgeniy Budaev <budaev.e@gmail.com>
2023-04-21 07:53:42 +0300

    feat: frontend. beforeunload

--- frontend/app/pages/Auth/Signup/Signup.tsx
+++ frontend/app/pages/Auth/Signup/Signup.tsx
@@ -1,3 +1,4 @@
+import {useEffect} from "react";
 import type { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "@remix-run/react";
@@ -35,6 +36,17 @@ export const Signup: FC = () => {
       }),
     });
   };
+
+  useEffect(() => {
+    const listener = (event: BeforeUnloadEvent) => {
+      if (Object.keys(form.methods.formState.dirtyFields).length === 0) {
+        return;
+      }
+      event.preventDefault();
+    }
+    window.addEventListener("beforeunload", listener);
+    return () => window.removeEventListener("beforeunload", listener)
+  }, [form.methods.formState.dirtyFields]);
 
   return (
     <section className="Signup">
