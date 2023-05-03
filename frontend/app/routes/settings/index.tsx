3e8e244695fc3ae30084bde8dee9ac8895c00bed
Evgeniy Budaev <budaev.e@gmail.com>
2023-05-03 15:25:54 +0300

    fix: frontend. убрал костыль

--- frontend/app/routes/settings/index.tsx
+++ frontend/app/routes/settings/index.tsx
@@ -2,7 +2,6 @@ import { json } from "@remix-run/node";
 import type { LoaderArgs, MetaFunction } from "@remix-run/node";
 import { Settings, settingsLinks } from "~/pages";
 import { getStoreFixedT } from "~/shared/store";
-import i18next from "i18next";
 
 export const loader = async (args: LoaderArgs) => {
   const { request } = args;
@@ -12,12 +11,8 @@ export const loader = async (args: LoaderArgs) => {
     title: t("pages.settings.meta.title"),
   });
 };
-let hydration = 0;
+
 export const meta: MetaFunction = ({ data }) => {
-  if (typeof window !== "undefined" && hydration) {
-    return { title: i18next.t("pages.settings.meta.title") || "Settings" };
-  }
-  hydration++;
   return { title: data?.title || "Settings" };
 };
 
