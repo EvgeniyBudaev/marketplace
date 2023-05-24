26ee836f24f4d128268c41cca43a04215ec29661
Evgeniy Budaev <budaev.e@gmail.com>
2023-05-24 09:01:43 +0300

    feat: frontend. Tooltip, admin product edit alias

--- frontend/app/uikit/Tooltip/utils/getTooltipOffset.ts
+++ frontend/app/uikit/Tooltip/utils/getTooltipOffset.ts
@@ -7,8 +7,8 @@ type TGetTooltipOffsetParams = {
 
 export const getTooltipOffset = ({ placement, referenceElement }: TGetTooltipOffsetParams) => {
   if (placement === "bottom" || placement === "top") {
-    return [0, referenceElement ? referenceElement.offsetHeight : 0];
+    return [0, 0];
   } else {
-    return [0, referenceElement ? referenceElement.offsetWidth : 0];
+    return [0, 0];
   }
 };
