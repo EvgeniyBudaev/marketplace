f076d43f73609b604c1cedef8e8164bfbf706b46
Evgeniy Budaev <budaev.e@gmail.com>
2023-07-29 19:53:28 +0300

    feat: frontend. update dependencies

--- frontend/remix.config.js
+++ frontend/remix.config.js
@@ -1,7 +1,14 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  serverDependenciesToBundle: ["crypto-random-string"],
+  serverDependenciesToBundle: [
+    "crypto-random-string",
+    /@ant-design\/*/,
+    /@antv\/*/,
+    /d3-interpolate/,
+    /d3-color/,
+    /lodash-es/,
+  ],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
