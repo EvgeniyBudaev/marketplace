b9cb14dd3cbc21e3f62b38e56e0e381e85c73e06
Evgeniy Budaev <budaev.e@gmail.com>
2024-03-18 18:44:04 +0300

    remix 2.8.0

--- frontend/server.ts
+++ frontend/server.ts
@@ -95,8 +95,8 @@ app.use("/build", express.static("public/build", { immutable: true, maxAge: "1y"
 // more aggressive with this caching.
 app.use(express.static("public", { maxAge: "1h" }));
 
-function loadBuild() {
-  let build = require(BUILD_DIR);
+async function loadBuild() {
+  let build = await import(BUILD_DIR);
   build = registerAccessTokenRefresh(
     build,
     {
@@ -114,12 +114,11 @@ app.all(
   "*",
   isProduction
     ? createRequestHandler({
-        build: loadBuild(),
+        build: () => loadBuild(),
       })
     : (...args) => {
-        purgeRequireCache();
         const requestHandler = createRequestHandler({
-          build: loadBuild(),
+          build: () => loadBuild(),
           mode: MODE,
         });
         return requestHandler(...args);
@@ -132,16 +131,3 @@ httpServer.listen(port, async () => {
   console.log(`Express server listening on port ${port}`);
   broadcastDevReady(await import(BUILD_DIR));
 });
-
-function purgeRequireCache() {
-  // purge require cache on requests for "server side HMR" this won't let
-  // you have in-memory objects between requests in development,
-  // alternatively you can set up nodemon/pm2-dev to restart the server on
-  // file changes, but then you'll have to reconnect to databases/etc on each
-  // change. We prefer the DX of this, so we've included it for you by default
-  for (let key in require.cache) {
-    if (key.startsWith(BUILD_DIR)) {
-      delete require.cache[key];
-    }
-  }
-}
